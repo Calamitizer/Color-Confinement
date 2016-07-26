30ce6a2536f4dc72b735823896e58beb4a78bba9
Calamitizer <cindersquall@gmail.com>
2016-07-26 16:06:51 -0400

    Let's try to get Angular routing set up (PGB is not pushing the html templates)

--- src/entry.js
+++ src/entry.js
@@ -1,17 +1,44 @@
 (function() {
     'use strict';
 
+    var app = {
+        initialize: function() {
+            this.bindEvents();
+        },
+        bindEvents: function() {
+            document.addEventListener('deviceready', this.onDeviceReady, true);
+        },
+        onDeviceReady: function() {
+            angular.element(document).ready(function() {
+                angular.bootstrap(document);
+            })
+        }
+    };
+
     var angular = require('angular');
     var Promise = require('bluebird');
     require('angular-route');
 
     console.log('past req');
 
+
     angular
         .module('cc', [
             'ngRoute',
             'cc.view'
         ]);
+
+    angular
+        .module('cc')
+        .config(compileConfig);
+
+    compileConfig.$inject = [
+        '$compileProvider'
+    ];
+
+    function compileConfig($compileProvider) {
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+    }
 
     angular
         .module('cc')
@@ -33,6 +60,8 @@
             });
     }
 
+
+
     /***********************************************************
      ***********************************************************/
 
@@ -47,4 +76,5 @@
         frameCtrl.tagline = 'foo!';
     }
 
+    app.initialize();
 }());
